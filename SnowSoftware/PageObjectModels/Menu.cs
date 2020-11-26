c49ff690bab6b6259dfcea15fcf85193b40ea4b9
PavelCodeStorage <paul.mail.dovgan@gmail.com>
2020-11-26 13:39:23 +0300

    added test examples

---
+++ SnowSoftware/PageObjectModels/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SnowSoftware.Driver;
+
+namespace SnowSoftware.PageObjectModels
+{
+    public class Menu
+    {
+       
+        private IWebDriver driver;
+        private WebDriverWait wait;
+      
+        private By successButton = By.XPath("//li[contains(@class,'menu-columns-3')][1]");
+        private By SnowGlobeCommunityButton = By.XPath("//li[@class='menu-item '][2]/a[contains(@href,'community')]");
+
+        public Menu()
+        {
+            driver = WebDriver.GetDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
+
+        [Obsolete]
+        public void ClickOnSuccessMenu()
+        {
+            IWebElement element = driver.FindElement(successButton);
+
+            Point successLocation = element.Location;
+
+            int x = successLocation.X;
+            int y = successLocation.Y;
+
+            Actions action = new Actions(driver);
+
+            action.MoveByOffset(x, y).Perform();
+
+            wait.Until(ExpectedConditions.ElementIsVisible(SnowGlobeCommunityButton));
+            driver.FindElement(SnowGlobeCommunityButton).Click();
+        } 
+}
+}
---
+++ SnowSoftware/SnowTests.cs
@@ -0,0 +1,53 @@
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using SnowSoftware.Driver;
+using SnowSoftware.PageObjectModels;
+
+namespace SnowSoftware
+{
+    [AllureNUnit]
+    public class Tests
+    {
+
+        private SitePage site;
+        private Menu menu;
+        private SnowGlobe snowGlobe;
+        private ReleaseNotes releaseNotes;
+
+        [SetUp]
+        public void Setup()
+        {
+            WebDriver.CreateDriver();
+            site = new SitePage();
+            menu = new Menu();
+            snowGlobe = new SnowGlobe();
+            releaseNotes = new ReleaseNotes();
+
+        }
+
+        [AllureTag("Smoke")]
+        [Test(Description = "Check Exist 'Snow License Manager 9.7.1 Release Note'")]
+        [System.Obsolete]
+        public void CheckReleaseNotes()
+        {
+            site.NavigateToTheSite("https://www.snowsoftware.com");
+            menu.ClickOnSuccessMenu();
+            snowGlobe.InputSearchField("Snow License Manager")
+                     .ClickReleaseNotes();
+
+            Assert.AreEqual(releaseNotes.getTitleText(), "Release Notes: Snow License Manager 9.7.1");
+            Assert.AreEqual(releaseNotes.getArticleNumber(), "000013119");
+        }
+
+
+        [TearDown]
+        public void After()
+        {
+            WebDriver.QuitDriver();
+        }
+    }
+}
